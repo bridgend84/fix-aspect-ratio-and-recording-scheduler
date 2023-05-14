50eacbe9345bad44098040a73b5fbd2eb0b826e4
bridgend84 <sacrun@gmail.com>
2023-05-14 13:40:19 +0200

    modify sh script with command line arguments and add batch script

--- fix-aspect-ratio/convert.sh
+++ fix-aspect-ratio/convert.sh
@@ -1,1 +1,1 @@
-ffmpeg -i m2-14954-08403200-09065300.mpg -map 0:v -map 0:v -map 0:a -c:v:0 libx264 -b:v:0 1.8M -vf:v:0 scale=iw:ih -c:v:1 libx264 -b:v:1 0.75M -s:v:1 480x360 -c:a:2 aac -b:a:2 128k output.mp4
+ffmpeg -i $1 -map 0:v -map 0:v -map 0:a -c:v:0 libx264 -b:v:0 1.8M -vf:v:0 scale=iw:ih -c:v:1 libx264 -b:v:1 0.75M -s:v:1 480x360 -c:a:2 aac -b:a:2 128k $2
