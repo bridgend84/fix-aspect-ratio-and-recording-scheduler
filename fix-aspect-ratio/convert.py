d884af001645de661316873fa00f337f7e5dda99
bridgend84 <sacrun@gmail.com>
2023-05-26 14:48:54 +0200

    refactor convert.py

--- fix-aspect-ratio/convert.py
+++ fix-aspect-ratio/convert.py
@@ -31,28 +31,28 @@ def process(fileURL):
         if len(io) != 2:
             print('\033[1;31;43m!!!SCRIPT ERROR!!! Illegal number of parameter in this line:' + ' '.join(io) + '\033[0;0m')
             continue
-        elif exists(io[0]) != True:
+        elif not exists(io[0]):
             print('\033[1;31;43m!!!SCRIPT ERROR!!! Input video file doesn\'t exists on this path:' + io[0] + '\033[0;0m')
             continue
         elif exists(io[1]):
             print('\033[1;31;43m!!!SCRIPT ERROR!!! Output video file exists on this path:' + io[0] + '\033[0;0m')
             continue
-        elif isdir(dirname(io[1])) != True:
+        elif not isdir(dirname(io[1])):
             print('\033[1;31;43m!!!SCRIPT ERROR!!! Output path doesn\'t exists:' + io[1] + '\033[0;0m')
             continue
-        elif access(dirname(io[1]), W_OK) != True:
+        elif not access(dirname(io[1]), W_OK):
             print('\033[1;31;43m!!!SCRIPT ERROR!!! Output path isn\'t writeable:' + io[1] + '\033[0;0m')
             continue
         else:
             result = convert(io[0], io[1])
-            if result.stderr != None:
+            if not result.stderr:
                 print(result.stderr)
             else:
                 print('\033[1;32;47m Video file converted succesfully to path: ' + io[1] + '\033[0;0m')
 
 if len(argv) != 2:
     print('\033[1;31;43m!!!SCRIPT ERROR!!! Illegal number of input parameter.\033[0;0m')
-elif exists(argv[1]) != True:
+elif not exists(argv[1]):
     print('\033[1;31;43m!!!SCRIPT ERROR!!! Input file doesn\'t exists.\033[0;0m')
 else:
     process(argv[1])
