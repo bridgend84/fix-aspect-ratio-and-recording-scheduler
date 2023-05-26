6652125d2fc7e1e98d726f725c555693e429df12
bridgend84 <sacrun@gmail.com>
2023-05-26 15:25:17 +0200

    refactor error logging (which line throw error)

--- fix-aspect-ratio/convert.py
+++ fix-aspect-ratio/convert.py
@@ -24,35 +24,37 @@ def convert(inputURL, outputURL):
 
 def process(fileURL):
     lines = []
+    counter = 0
     with open(fileURL, 'r', encoding='utf8') as file:
         for line in file:
             lines.append(line.split())
     for io in lines:
+        counter += 1
         if len(io) != 2:
-            print('\033[1;31;43m!!!SCRIPT ERROR!!! Illegal number of parameter in this line:' + ' '.join(io) + '\033[0;0m')
+            print('\033[1;31;43m!!!SCRIPT ERROR!!! Illegal number of parameter in line ' + str(counter) + ': ' + ' '.join(io) + ' \033[0;0m')
             continue
         elif not exists(io[0]):
-            print('\033[1;31;43m!!!SCRIPT ERROR!!! Input video file doesn\'t exists on this path:' + io[0] + '\033[0;0m')
+            print('\033[1;31;43m!!!SCRIPT ERROR!!! Input video file doesn\'t exists on this path (line ' + str(counter) + '): ' + io[0] + ' \033[0;0m')
             continue
         elif exists(io[1]):
-            print('\033[1;31;43m!!!SCRIPT ERROR!!! Output video file exists on this path:' + io[0] + '\033[0;0m')
+            print('\033[1;31;43m!!!SCRIPT ERROR!!! Output video file already exists on this path (line ' + str(counter) + '): ' + io[1] + ' \033[0;0m')
             continue
         elif not isdir(dirname(io[1])):
-            print('\033[1;31;43m!!!SCRIPT ERROR!!! Output path doesn\'t exists:' + io[1] + '\033[0;0m')
+            print('\033[1;31;43m!!!SCRIPT ERROR!!! Output folder doesn\'t exists (line ' + str(counter) + '): ' + dirname(io[1]) + ' \033[0;0m')
             continue
         elif not access(dirname(io[1]), W_OK):
-            print('\033[1;31;43m!!!SCRIPT ERROR!!! Output path isn\'t writeable:' + io[1] + '\033[0;0m')
+            print('\033[1;31;43m!!!SCRIPT ERROR!!! Output folder isn\'t writeable (line ' + str(counter) + '): ' + dirname(io[1]) + ' \033[0;0m')
             continue
         else:
             result = convert(io[0], io[1])
-            if not result.stderr:
+            if result.stderr != None:
                 print(result.stderr)
             else:
-                print('\033[1;32;47m Video file converted succesfully to path: ' + io[1] + '\033[0;0m')
+                print('\033[1;32;47m Video file converted succesfully to path: ' + io[1] + ' \033[0;0m')
 
 if len(argv) != 2:
-    print('\033[1;31;43m!!!SCRIPT ERROR!!! Illegal number of input parameter.\033[0;0m')
+    print('\033[1;31;43m!!!SCRIPT ERROR!!! Illegal number of input parameter \033[0;0m')
 elif not exists(argv[1]):
-    print('\033[1;31;43m!!!SCRIPT ERROR!!! Input file doesn\'t exists.\033[0;0m')
+    print('\033[1;31;43m!!!SCRIPT ERROR!!! Input file doesn\'t exists \033[0;0m')
 else:
     process(argv[1])
